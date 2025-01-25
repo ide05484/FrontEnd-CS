49c00772cb5b77d668339c20ef9bfb52d8ce454c
Eunicekk <kate4234@gmail.com>
2025-01-25 22:00:25 +0900

    20250125 [6장] 6.0 Add

---
+++ 모던 리액트 Deep Dive/6장. 리액트 개발 도구로 디버깅하기/debug-test/src/App.tsx
@@ -0,0 +1,50 @@
+import { ChangeEvent, useEffect, useState } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+  const [number, setNumber] = useState(0);
+  const [list, setList] = useState([
+    { name: "apple", amount: 5000 },
+    { name: "orange", amount: 1000 },
+    { name: "watermelon", amount: 1500 },
+    { name: "pineapple", amount: 500 },
+  ]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("surprise!");
+      setText("1000");
+    }, 3000);
+  });
+
+  function handleTextChange(e: ChangeEvent<HTMLInputElement>) {
+    setText(e.target.value);
+  }
+
+  function handleSubmit() {
+    setList((prev) => [...prev, { name: text, amount: number }]);
+  }
+
+  function handleNumberChange(e: ChangeEvent<HTMLInputElement>) {
+    setNumber(e.target.valueAsNumber);
+  }
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={handleTextChange} />
+      <button onClick={handleSubmit}>추가</button>
+
+      <input type="number" value={number} onChange={handleNumberChange} />
+
+      <ul>
+        {list.map((value, key) => (
+          <li key={key}>
+            {value.name} {value.amount}원
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
